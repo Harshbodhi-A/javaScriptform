02c372223f27ef231acd1e18fb67ae9faa972364
Aniket <aniketharshbodhi227@gmail.com>
2024-05-08 20:06:04 +0530

    popup window

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
+// document.getElementById("myForm").addEventListener("submit", function(event) {
+//     event.preventDefault(); 
     
 
+//     var email = document.getElementById("exampleInputEmail1").value;
+//     var password = document.getElementById("exampleInputPassword1").value;
+//     var isChecked = document.getElementById("exampleCheck1").checked;
+
+   
+//     var displayDiv = document.getElementById("displayData");
+//     displayDiv.innerHTML = "<h2>Form Data</h2>" +
+//                             "<p>Email: " + email + "</p>" +
+//                             "<p>Password: " + password + "</p>" +
+//                             "<p>Checkbox Checked: " + isChecked + "</p>";
+
+    
+//     console.log("Email:", email);
+//     console.log("Password:", password);
+//     console.log("Checkbox Checked:", isChecked);
+// });
+function openPopup() {
     var email = document.getElementById("exampleInputEmail1").value;
     var password = document.getElementById("exampleInputPassword1").value;
     var isChecked = document.getElementById("exampleCheck1").checked;
 
-   
-    var displayDiv = document.getElementById("displayData");
-    displayDiv.innerHTML = "<h2>Form Data</h2>" +
-                            "<p>Email: " + email + "</p>" +
-                            "<p>Password: " + password + "</p>" +
-                            "<p>Checkbox Checked: " + isChecked + "</p>";
-
-    
-    console.log("Email:", email);
-    console.log("Password:", password);
-    console.log("Checkbox Checked:", isChecked);
-});
+    var popupWindow = window.open("", "_blank", "width=400,height=300");
+    popupWindow.document.write("<h2>Login Details</h2>" +
+                                "<p>Email: " + email + "</p>" +
+                                "<p>Password: " + password + "</p>" +
+                                "<p>Checkbox Checked: " + isChecked + "</p>");
+}
